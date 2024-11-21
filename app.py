ef4972fa4afdf4c1ec287acd31a4cce9017c006f
vamshi0903 <vamshidhar127@gmail.com>
2024-11-21 16:31:36 -0600

    computational problem solved

--- app.py
+++ app.py
@@ -1,12 +1,60 @@
-# app.py
+# # app.py
+
+# import streamlit as st
+# import pandas as pd
+# import model  # Importing model.py
+
+# # Load data and models
+# movies = model.load_and_process_data()
+# movies, models_and_encoders = model.create_cluster_models(movies)
+
+# st.title('Movie Recommendation System')
+# st.write('Get movie recommendations based on your favorite movies!')
+
+# # Option to choose recommendation method
+# option = st.selectbox(
+#     'Choose a recommendation method:',
+#     ('Genres', 'Keywords', 'Cast')
+# )
+
+# # Movie selection
+# movie_titles = movies['title'].tolist()
+# selected_movie_title = st.selectbox('Select a movie:', movie_titles)
+
+# # Number of recommendations
+# num_recommendations = st.slider('Number of recommendations:', min_value=1, max_value=20, value=5)
+
+# # Get Recommendations button
+# if st.button('Get Recommendations'):
+#     if option == 'Genres':
+#         recommendations = model.recommendationsOnGenres(selected_movie_title, movies, num_recommendations)
+#     elif option == 'Keywords':
+#         recommendations = model.recommendationsOnKeywords(selected_movie_title, movies, num_recommendations)
+#     elif option == 'Cast':
+#         recommendations = model.recommendationsOnCast(selected_movie_title, movies, num_recommendations)
+#     else:
+#         recommendations = pd.DataFrame()
+
+#     if not recommendations.empty:
+#         st.write('Recommendations:')
+#         st.table(recommendations[['title']])
+#     else:
+#         st.write('No recommendations found.')
+
+
 
 import streamlit as st
 import pandas as pd
-import model  # Importing model.py
+import model  # Import recommendation functions
+import os
 
-# Load data and models
-movies = model.load_and_process_data()
-movies, models_and_encoders = model.create_cluster_models(movies)
+@st.cache_data
+def load_movies():
+    # Load the precomputed movies DataFrame
+    movies = pd.read_csv('movies_with_clusters.csv')
+    return movies
+
+movies = load_movies()
 
 st.title('Movie Recommendation System')
 st.write('Get movie recommendations based on your favorite movies!')
@@ -37,6 +85,7 @@ if st.button('Get Recommendations'):
 
     if not recommendations.empty:
         st.write('Recommendations:')
-        st.table(recommendations[['title']])
+        st.table(recommendations[['title', 'popularity']])
     else:
         st.write('No recommendations found.')
+
--- model.py
+++ model.py
@@ -1,13 +1,97 @@
 # model.py
 
+# import pandas as pd
+# import numpy as np
+# from sklearn.preprocessing import MultiLabelBinarizer
+# from sklearn.cluster import KMeans
+# from ast import literal_eval
+# import streamlit as st  # Import Streamlit to use caching decorators
+
+# @st.cache_data
+# def load_and_process_data():
+#     # Load and merge the data
+#     dataset1 = pd.read_csv('tmdb_5000_credits.csv')
+#     dataset2 = pd.read_csv('tmdb_5000_movies.csv')
+
+#     dataset1.columns = ['id', 'title', 'cast', 'crew']
+#     movies = dataset2.merge(dataset1, on='id')
+
+#     # Rename columns to resolve conflicts
+#     movies.rename(columns={'title_x': 'title'}, inplace=True)
+#     movies.drop(columns=['title_y'], inplace=True)
+
+#     # Define helper functions
+#     def extractFeature(obj):
+#         if isinstance(obj, str):
+#             obj = literal_eval(obj)
+#         if isinstance(obj, list):
+#             return [d['name'] for d in obj]
+#         return []
+
+#     def topCastNames(cast_list, top_n=5):
+#         if isinstance(cast_list, list):
+#             names = [member['name'] for member in cast_list[:top_n]]
+#             return names
+#         return []
+
+#     # Process features
+#     movies['genres'] = movies['genres'].apply(extractFeature)
+#     movies['keywords'] = movies['keywords'].apply(extractFeature)
+#     movies['cast'] = movies['cast'].apply(literal_eval)
+#     movies['castNames'] = movies['cast'].apply(topCastNames)
+
+#     return movies
+
+# @st.cache_resource
+# def create_cluster_models(movies, k=100):
+#     # Prepare data for clustering
+#     genres = movies['genres']
+#     keywords = movies['keywords']
+#     cast = movies['castNames']
+
+#     # Encode features
+#     genreMlb = MultiLabelBinarizer()
+#     encodedGenres = genreMlb.fit_transform(genres)
+
+#     keywordsMlb = MultiLabelBinarizer()
+#     encodedKeywords = keywordsMlb.fit_transform(keywords)
+
+#     castMlb = MultiLabelBinarizer()
+#     encodedCast = castMlb.fit_transform(cast)
+
+#     # Create and fit KMeans models
+#     genresKmeans = KMeans(n_clusters=k, random_state=42, n_init='auto')
+#     genresKmeans.fit(encodedGenres)
+
+#     keywordsKmeans = KMeans(n_clusters=k, random_state=42, n_init='auto')
+#     keywordsKmeans.fit(encodedKeywords)
+
+#     castKmeans = KMeans(n_clusters=k, random_state=42, n_init='auto')
+#     castKmeans.fit(encodedCast)
+
+#     # Assign cluster labels to movies
+#     movies['genresClusters'] = genresKmeans.labels_
+#     movies['keywordsClusters'] = keywordsKmeans.labels_
+#     movies['castClusters'] = castKmeans.labels_
+
+#     # Optional: Store encoders and models
+#     models_and_encoders = {
+#         'genres': {'kmeans': genresKmeans, 'mlb': genreMlb},
+#         'keywords': {'kmeans': keywordsKmeans, 'mlb': keywordsMlb},
+#         'cast': {'kmeans': castKmeans, 'mlb': castMlb}
+#     }
+
+#     return movies, models_and_encoders
+
+# model.py
+
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.cluster import KMeans
 from ast import literal_eval
-import streamlit as st  # Import Streamlit to use caching decorators
+import pickle
 
-@st.cache_data
 def load_and_process_data():
     # Load and merge the data
     dataset1 = pd.read_csv('tmdb_5000_credits.csv')
@@ -42,7 +126,6 @@ def load_and_process_data():
 
     return movies
 
-@st.cache_resource
 def create_cluster_models(movies, k=100):
     # Prepare data for clustering
     genres = movies['genres']
@@ -74,14 +157,13 @@ def create_cluster_models(movies, k=100):
     movies['keywordsClusters'] = keywordsKmeans.labels_
     movies['castClusters'] = castKmeans.labels_
 
-    # Optional: Store encoders and models
-    models_and_encoders = {
-        'genres': {'kmeans': genresKmeans, 'mlb': genreMlb},
-        'keywords': {'kmeans': keywordsKmeans, 'mlb': keywordsMlb},
-        'cast': {'kmeans': castKmeans, 'mlb': castMlb}
-    }
+    return movies
 
-    return movies, models_and_encoders
+def save_precomputed_data(movies):
+    # Save the movies DataFrame with cluster labels
+    movies.to_csv('movies_with_clusters.csv', index=False)
+
+
 
 # Recommendation functions remain the same
 def recommendationsOnGenres(movieTitle, movies, count=5):
@@ -116,3 +198,9 @@ def recommendationsOnCast(movieTitle, movies, count=5):
     recommendations = clusterMovies[clusterMovies['title'] != movieTitle]
     recommendations = recommendations.sort_values(by='popularity', ascending=False)
     return recommendations.head(count)
+
+# Run this part only when computing clusters offline
+if __name__ == '__main__':
+    movies = load_and_process_data()
+    movies = create_cluster_models(movies)
+    save_precomputed_data(movies)
